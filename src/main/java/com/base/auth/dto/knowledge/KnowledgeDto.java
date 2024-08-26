b2771826042634505daadf602e00a9b09bb14e14
Hiep <hoanghiep8401@gmail.com>
2024-08-26 19:03:20 +0700

    Remove unnecessary files and directories, keep only src

--- src/main/java/com/base/auth/dto/knowledge/KnowledgeDto.java
+++ src/main/java/com/base/auth/dto/knowledge/KnowledgeDto.java
@@ -7,8 +7,6 @@ import java.util.Date;
 
 @Data
 public class KnowledgeDto extends ABasicAdminDto {
-    @ApiModelProperty(name = "id")
-    private Long id;
     @ApiModelProperty(name = "name")
     private String name;
     @ApiModelProperty(name = "content")
